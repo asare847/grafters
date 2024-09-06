3da3974f00a4110eb5c8d0121cadbcdcec007d43
asare847 <gideonasare847@outlook.com>
2024-09-06 06:25:26 +0100

    company model added

---
+++ database/migrations/2024_09_06_043114_create_sites_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sites', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('address');
+            $table->string('phone');
+            $table->text('description');
+            $table->foreignId('company_id');
+            $table->foreign('company_id')->references('id')->on('users')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sites');
+    }
+};
